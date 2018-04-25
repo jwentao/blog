0e4fd8a498845e6718ab1213a23aed22a23af8db
jinwt <380793594@qq.com>
2018-04-25 18:03:49 +0800

    add html loader

--- fontend/build/webpack.common.conf.js
+++ fontend/build/webpack.common.conf.js
@@ -7,164 +7,176 @@ const path = require('path')
 const webpack = require('webpack')
 
 const generateConfig = env => {
-    const extractLess = new ExtractTextWebpackPlugin({
-        filename: 'css/[name]-bundle-[hash:5].css',
-    })
+	const extractLess = new ExtractTextWebpackPlugin({
+		filename: 'css/[name]-bundle-[hash:5].css',
+	})
 
-    const scriptLoader = ['babel-loader']
-        .concat(env === 'production' 
-            ? []
-            : [
-            //   {
-            //     loader: 'eslint-loader',
-            //     options: {
-            //         formatter: require('eslint-friendly-formatter')
-            //     }
-            // }
-            ]
-        )
-    const cssLoaders = [
-        {
-            loader: 'css-loader',
-            options: {
-                importLoaders: 2,
-                sourceMap: env === 'development'
-            }
-        },
-        {
-            loader: 'postcss-loader',
-            options: {
-                ident: 'postcss',
-                sourceMap: env === 'development',
-                plugins: [  
-                    require('postcss-cssnext')()
-                ].concat(
-                    env === 'production' 
-                    ? require('postcss-sprites')({
-                        spritePath: 'dist/assets/imgs/sprites',
-                        retina: true
-                    })
-                    : []
-                )
-            }
-        },
-        {
-            loader: 'sass-loader',
-            options: {
-                sourceMap: env === 'development'
-            }
-        }
-    ]
-    const styleLoader = env === 'production'
-        ? extractLess.extract({
-            fallback: 'style-loader',
-            use: cssLoaders
-        })
-        : [{
-            loader: 'style-loader'
-        }].concat(cssLoaders)
+	const scriptLoader = ['babel-loader']
+		.concat(env === 'production'
+			? []
+			: [
+				//   {
+				//     loader: 'eslint-loader',
+				//     options: {
+				//         formatter: require('eslint-friendly-formatter')
+				//     }
+				// }
+			]
+		)
+	const cssLoaders = [
+		{
+			loader: 'css-loader',
+			options: {
+				importLoaders: 2,
+				sourceMap: env === 'development'
+			}
+		},
+		{
+			loader: 'postcss-loader',
+			options: {
+				ident: 'postcss',
+				sourceMap: env === 'development',
+				plugins: [
+					require('postcss-cssnext')()
+				].concat(
+					env === 'production'
+						? require('postcss-sprites')({
+						spritePath: 'dist/assets/imgs/sprites',
+						retina: true
+					})
+						: []
+				)
+			}
+		},
+		{
+			loader: 'sass-loader',
+			options: {
+				sourceMap: env === 'development'
+			}
+		}
+	]
+	const styleLoader = env === 'production'
+		? extractLess.extract({
+			fallback: 'style-loader',
+			use: cssLoaders
+		})
+		: [{
+			loader: 'style-loader'
+		}].concat(cssLoaders)
 
-    const fileLoader = path => {
-        return env === 'development' 
-        ? [{
-            loader: 'file-loader',
-            options: {
-                name: '[name]-[hash:5].[ext]',
-                outputPath: path
-            }
-        }]
-        : [{
-            loader: 'url-loader',
-            options: {
-                name: '[name]-[hash:5].[ext]',
-                limit: 1000,
-                outputPath: path
-            }
-        }]
-    }
-    
-    return {
-        entry: {
-            app: './src/app.js',
-            detail: './src/detail.js'
-        },
+	const fileLoader = path => {
+		return env === 'development'
+			? [{
+				loader: 'file-loader',
+				options: {
+					name: '[name]-[hash:5].[ext]',
+					outputPath: path
+				}
+			}]
+			: [{
+				loader: 'url-loader',
+				options: {
+					name: '[name]-[hash:5].[ext]',
+					limit: 1000,
+					outputPath: path
+				}
+			}]
+	}
 
-        output: {
-            path: path.resolve(__dirname, '../dist'),
-            publicPath: '/',
-            filename: 'js/[name]-bundle-[hash:5].js'
-        },
+	return {
+		entry: {
+			app: './src/app.js',
+			detail: './src/detail.js'
+		},
 
-        resolve: {
-            alias: {
-                jquery$: path.resolve(__dirname, '../src/libs/jquery.min.js')
-            }
-        },
+		output: {
+			path: path.resolve(__dirname, '../dist'),
+			publicPath: '/',
+			filename: 'js/[name]-bundle-[hash:5].js'
+		},
 
-        module: {
-            rules: [
-                {
-                    test:/\.js$/,
-                    include: [path.resolve(__dirname, '../src')],
-                    exclude: [path.resolve(__dirname, '../src/libs')],
-                    use: scriptLoader
-                },
+		resolve: {
+			alias: {
+				jquery$: path.resolve(__dirname, '../src/libs/jquery.min.js')
+			}
+		},
 
-                {
-                    test: /\.scss$/,
-                    use: styleLoader
-                },
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					include: [path.resolve(__dirname, '../src')],
+					exclude: [path.resolve(__dirname, '../src/libs')],
+					use: scriptLoader
+				},
 
-                {
-                    test: /\.(png|jpg|jpeg|gif)$/,
-                    use: fileLoader('assets/imgs/').concat(
-                        env === 'production' 
-                        ? {
-                            loader: 'img-loader',
-                            options: {
-                                pngquant: {
-                                    quality: 80
-                                }
-                            }
-                        }
-                        : []
-                    )
-                },
+				{
+					test: /\.scss$/,
+					use: styleLoader
+				},
 
-                {
-                    test:/\.(eot|woff2?|ttf|svg)$/,
-                    use: fileLoader('assets/imgs/')
-                }
-            ]
-        },
+				{
+					test: /\.(png|jpg|jpeg|gif)$/,
+					use: fileLoader('assets/imgs/').concat(
+						env === 'production'
+							? {
+							loader: 'img-loader',
+							options: {
+								pngquant: {
+									quality: 80
+								}
+							}
+						}
+							: []
+					)
+				},
 
-        plugins: [
-            extractLess,
+				{
+					test: /\.(eot|woff2?|ttf|svg)$/,
+					use: fileLoader('assets/imgs/')
+				},
 
-            new HtmlWebpackPlugin({
-                filename: 'index.html',
-                template: './index.html',
-                minify: {
-                    collapseWhitespace: true
-                },
-                chunks: ['app']
-            }),
-              new HtmlWebpackPlugin({
-                filename: 'detail.html',
-                template: './detail.html',
-                minify: {
-                  collapseWhitespace: true
-                },
-                chunks: ['detail']
-              })
-        ]
-    }
+				{
+					test: /\.html$/,
+					use: [
+						{
+							loader: 'html-loader',
+							options: {
+								attrs: ['img:src', 'img:data-src']
+							}
+						}
+					]
+				}
+			]
+		},
+
+		plugins: [
+			extractLess,
+
+			new HtmlWebpackPlugin({
+				filename: 'index.html',
+				template: './index.html',
+				minify: {
+					collapseWhitespace: true
+				},
+				chunks: ['app']
+			}),
+			new HtmlWebpackPlugin({
+				filename: 'detail.html',
+				template: './detail.html',
+				minify: {
+					collapseWhitespace: true
+				},
+				chunks: ['detail']
+			})
+		]
+	}
 }
 
 module.exports = env => {
-    let config = env === 'production'
-        ? productionConfig
-        : developmentConfig
+	let config = env === 'production'
+		? productionConfig
+		: developmentConfig
 
-    return merge(generateConfig(env), config)
+	return merge(generateConfig(env), config)
 }
