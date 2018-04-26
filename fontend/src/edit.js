ac875c2e68cee798da6292d8ac2d18ba406fc102
jwentao <380793594@qq.com>
2018-04-27 00:43:14 +0800

    try use tab in textarea 2018/4/27

--- fontend/src/edit.js
+++ fontend/src/edit.js
@@ -1,53 +1,65 @@
 import './css/edit.scss'
 import { $ } from './js/util'
-
+// 引入showdown 和 showdown高亮
 let showdown  = require('showdown')
 let showdownhighlight = require('showdown-highlight');
-// let converter = new showdown.Converter()
-
+// 创建shoudown对象
 let converter = new showdown.Converter({
-	// That's it
 	extensions: [showdownhighlight]
 });
+
 let inputText = $('#text-input')[0]
 let contentText = $('#content')[0]
 inputText.addEventListener('keyup', e => {
 	contentText.innerHTML = converter.makeHtml(inputText.value)
 	console.log( converter.makeHtml(inputText.value))
 }, false)
+
+HTMLTextAreaElement.prototype.getCaretPosition = function () {
+//return the caret position of the textarea
+    return this.selectionStart;
+};
+HTMLTextAreaElement.prototype.setCaretPosition = function (position) {
+//change the caret position of the textarea
+    this.selectionStart = position;
+    this.selectionEnd = position;
+    this.focus();
+};
+HTMLTextAreaElement.prototype.hasSelection = function () {
+//if the textarea has selection then return true
+    if (this.selectionStart == this.selectionEnd) {
+        return false;
+    } else {
+        return true;
+    }
+};
+HTMLTextAreaElement.prototype.getSelectedText = function () {
+//return the selection text
+    return this.value.substring(this.selectionStart, this.selectionEnd);
+};
+HTMLTextAreaElement.prototype.setSelection = function (start, end) {
+//change the selection area of the textarea
+    this.selectionStart = start;
+    this.selectionEnd = end;
+    this.focus();
+};
+let textarea = inputText
 inputText.addEventListener('keydown', e => {
 
 	if (event.key !== 'Tab') return
-	if (event && event.preventDefault) {
-		console.log('a')
-		event.preventDefault()
-	} else {
-		window.event.returnValue = false
-	}
-    inputText.value += '    '
-	// // 获取光标的range对象 event.view 是一个window对象
-	// let range = event.view.getSelection().getRangeAt(0);
-	// // 光标的偏移位置
-	// let offset = range.startOffset;
-	// // 新建一个span元素
-	// let span = document.createElement('span');
-	// // 四个 表示四个空格
-	// span.innerHTML = '    ';
-	// // 创建一个新的range对象
-	// let newrange = document.createRange();
-	// // 设置新的range的位置，也是插入元素的位置
-	// newrange.setStart(range.startContainer, offset);
-	// newrange.setEnd(range.startContainer, offset);
-	// newrange.collapse(true);
-	// newrange.insertNode(span);
-	// // // 去掉旧的range对象，用新的range对象替换
-	// // event.view.getSelection().removeAllRanges();
-	// event.view.getSelection().addRange(range);
-	// // // 将光标的位置向后移动一个偏移量，放到加入的四个空格后面
-	// range.setStart(span, 1);
-	// // range.setEnd(span, 1);
+	// if (event && event.preventDefault) {
+	// 	console.log('a')
+	// 	e.preventDefault()
+	// } else {
+	// 	window.event.returnValue = false
+	// }
+    var newCaretPosition;
+    newCaretPosition = textarea.getCaretPosition() + "  ".length;
+    console.log(newCaretPosition)
+    textarea.value = textarea.value.substring(0, textarea.getCaretPosition()) + " " + textarea.value.substring(textarea.getCaretPosition(), textarea.value.length);
+    textarea.setCaretPosition(newCaretPosition);
+    e.preventDefault()
+    return false;
 }, false)
-
-let tab = e => {console.log(e)}
 
 
