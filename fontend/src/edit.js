817537d0f3a397da89d0be20ecc870b980b33d44
jwentao <380793594@qq.com>
2018-05-09 00:20:37 +0800

    develop edit 2018/5/9

--- fontend/src/edit.js
+++ fontend/src/edit.js
@@ -8,10 +8,20 @@ let converter = new showdown.Converter({
 	extensions: [showdownhighlight]
 });
 
+let postInfo = {
+	title: '', // 标题
+    auth: '', // 作者
+	tag: '', // 标签
+	type: '1', // 1为原创，2为转载
+    origin_article: '', // 原文(markdowm语法)
+    trans_article: '' // 转换后的文章(html语法)
+}
+
 let inputText = $('#text-input')
 let contentText = $('#content')
+let text = '';
 inputText.addEventListener('keyup', e => {
-	let text = converter.makeHtml(inputText.value)
+	text = converter.makeHtml(inputText.value)
 	// showdown解析四个空格开头的代码块，会出现hljs字符，这里先暴力替换掉
 	text = text.replace(/>hljs/g, '>')
 	// 序列化h1的id
@@ -71,7 +81,7 @@ inputText.addEventListener('keydown', e => {
     e.preventDefault()
     return false;
 }, false)
-
+// 显示/隐藏 发布box
 $('#toggle-submit-box').addEventListener('click', e => {
 	let el = $('#toggle-submit-box');
 	let panel = $('#panel');
@@ -85,26 +95,30 @@ $('#toggle-submit-box').addEventListener('click', e => {
 		panel.style.display = 'none';
 	}
 }, false)
-
+// 切换发布box里的标签
 let tagList = Array.from($('.class-item'))
 tagList.forEach(item => {
 	item.addEventListener('click', e => {
+		postInfo.type = e.target.dataset.type
 		tagList.forEach(i => {
 			i.classList.remove('active')
 		})
 		item.classList.add('active')
 	}, false)
 })
-
-async function postArticle () {
-	let data = await ajax('http://localhost/article/post_article', {method: 'post', body: {
-		'title1': '一个测试的标题1',
-		'auth': '测试作者名',
-		'tag': 'tag1,tag2,tag3',
-		'type': 'origin',
-		'origin_article': '####大量的文本####大量的文本####大量的文本####大量的文本####大量的文本####大量的文本####大量的文本####大量的文本####大量的文本####大量的文本####大量的文本####大量的文本####大量的文本####大量的文本####大量的文本####大量的文本####大量的文本####大量的文本####大量的文本####大量的文本####大量的文本####大量的文本####大量的文本####大量的文本',
-		'trans_article': `<h4>大量的文本</h4><h4>大量的文本</h4><h4>大量的文本</h4><h4>大量的文本</h4><h4>大量的文本</h4><h4>大量的文本</h4><h4>大量的文本</h4><h4>大量的文本</h4><h4>大量的文本</h4><h4>大量的文本</h4><h4>大量的文本</h4><h4>大量的文本</h4><h4>大量的文本</h4><h4>大量的文本</h4><h4>大量的文本</h4>`
-	}})
+// 点击确认发布
+$('#confirm').addEventListener('click', e => {
+	postInfo.title = $('#title-value').value;
+	postInfo.auth = $('#auth-value').value;
+	postInfo.tag = $('#tag-value').value;
+	postInfo.origin_article = inputText.value;
+	postInfo.trans_article = text;
+	console.log(postInfo)
+    postArticle(postInfo)
+}, false)
+async function postArticle (postInfo) {
+	let data = await ajax('http://localhost/article/post_article',
+		{method: 'post', body: postInfo})
 	console.log(data)
 }
 // postArticle()
