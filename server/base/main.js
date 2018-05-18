17dbf75f23a027a142317f6419956ab8f7d8ae85
jinwt <380793594@qq.com>
2018-05-18 15:20:37 +0800

    develop verify 2018/5/18

--- server/base/main.js
+++ server/base/main.js
@@ -1,19 +1,37 @@
 const _config = require('./config');
 const jwt = require("jsonwebtoken");
+const mongo = require('./mongo');
 let verify = function (token) {
-    return jwt.verify(token, _config.secretOrPrivateKey, (err, decoded) => {
-        console.log('in verify')
-        console.log(err)
-        if (err) {
-            return false;
-        } else {
-            console.log('success', decoded)
-            // 如果验证通过，在req中写入解密结果
-            // req.decoded = decoded;
-            //console.log(decoded)  ;
-            return (decoded);
-            // next(); //继续下一步路由
-        }
+    return new Promise((resolve, reject) => {
+			jwt.verify(token, _config.secretOrPrivateKey, (err, decoded) => {
+				console.log('in verify')
+				console.log(err)
+				// 解密失败，返回false
+				if (err) {
+					return resolve(false);
+				} else {
+					console.log('success', decoded)
+					let now = new Date().getTime();
+					console.log('now')
+					// 超出时间，返回false
+					if (now > decoded.expires) {
+						console.log('超时');
+						return resolve(false);
+					}
+					mongo.findOne('user_info', {id: decoded.id, psw: decoded.psw}, (err, res) => {
+						if (res) {
+							// 查询到
+							console.log('账号密码正确');
+							return resolve(true);
+						} else {
+							// 未查询到
+							console.log('账号密码错误')
+							return resolve(false);
+						}
+					});
+					// next(); //继续下一步路由
+				}
+			});
     });
 };
 
